f2c94bcf0003705562aaf473f81fbb9e6d0329fe
Julián Dorao <133274347+Juliandavidlp@users.noreply.github.com>
2024-06-26 10:27:05 -0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,29 @@
+from flask import jsonify
+from app.models import products
+
+#Una función del módulo flask para convertir a formato json listas y diccionarios de python
+
+def index():
+    response = {'message':'Hola mundo API FLASK'}
+    return jsonify(response)
+
+
+"""Un crud de productos"""
+#Función que busca un producto
+
+def get_product():
+    pass
+
+#Función que busca todo el listado de productos
+def get_all_products():
+    return jsonify(products)
+
+def create_product():
+    pass
+
+def update_product():
+    pass
+
+def delete_product():
+    pass
+
