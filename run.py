07ebcbe17a46ca4c13d0fbc9c816f5e551765761
Julián Dorao <133274347+Juliandavidlp@users.noreply.github.com>
2024-06-25 21:12:32 -0300

    Julián

---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import Flask
+#Importo un objeto de la clase flask
+from app.views import *
+#Importo todas las funciones desde las vistas
+
+#Crear una instancia de Flask
+app = Flask(__name__)
+
+#Asociación de las rutas con las vistas, el método correspondiente
+app.route('/',methods=['GET'])(index)
+app.route('/api/products/',methods=['GET'])(get_all_products)
+
+#Permite separar el código que se ejecuta cuando se corre el archivo
+if __name__=='__main__':
+    app.run(debug=True)
