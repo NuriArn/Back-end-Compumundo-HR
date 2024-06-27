d235b9cf7c731a392c017d62718cbdeb07479aff
Julián <juliandorao@gmail.com>
2024-06-27 20:09:25 -0300

    Julián

--- app/models.py
+++ app/models.py
@@ -1,52 +1,32 @@
+from app.database import get_db
+
+
 class Product:
-    def __init__(self):
-        self.id_product
-        self.title
-        self.price
-        self.release_date
-        self.banner
+    #Método constructor
+    def __init__(self,id_product=None, title=None,price=None,release_date=None, banner=None):
+        self.id_product = id_product
+        self.title = title
+        self.price = price
+        self.release_date = release_date
+        self.banner = banner
 
-def save(self):
-    #Lógica para el INSERT/UPDATE en la base de datos
-    pass
 
-def delete(self):
-    #Lógica para hacer un DELETE en la base de datos
-    pass
+    @staticmethod
+    def get_all(): #Lógica para traer los productos
+        db = get_db()
+        cursor = db.cursor() #cursor permite ejecutar ciertas instrucciones de la base de datos y extraer el resultado de esa consulta o de la conexión con la bd.
+        cursor.execute("SELECT * FROM products")
+        rows = cursor.fetchall() 
 
-def get_all(self):
-    #Lógica para traer todos los productos
-    pass
+        return rows
 
-products = [
-    {
-        'id_product':1,
-        'title':'Product',
-        'price':'Christopher Nolan',
-        'banner':'inception_banner.jpg'
-    },
-    {
-        'id_product':2,
-        'title':'The Godfather',
-        'price':'Francis Ford Coppola',
-        'banner':'godfather_banner.jpg'
-    },
-    {
-        'id_product':3,
-        'title':'Forrest Gump',
-        'price':'Robert Zemeckis',
-        'banner':'forrestgump_banner.jpg'
-    },
-    {
-        'id_product':4,
-        'title':'Titanic',
-        'price':'James Cameron',
-        'banner':'titanic_banner.jpg'
-    },
-    {
-        'id_product':5,
-        'title':'Gladiator',
-        'price':'Ridley Scott',
-        'banner':'gladiator_banner.jpg'
-    }
-]
+    def save(self):
+        #Lógica para el INSERT/UPDATE en la base de datos
+        pass
+
+    def delete(self):
+        #Lógica para hacer un DELETE en la base de datos
+        pass
+
+    
+
